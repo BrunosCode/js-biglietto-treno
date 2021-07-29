46d30fe8577be09b545a6d95714208e30c9d38f6
brunEdo <edobruno.m@gmail.com>
2021-07-29 14:14:20 +0200

    js steps

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// TICKET PRICE CALCULATOR
+// ask and store user age
+// ask and store distance of the destination
+// store price moltiplicator, minor discount and over 65 discount
+// if user is a minor apply discount
+// if user age is over 65 apply discount
+// return the price
+// show price to the user
