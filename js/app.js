5d2c3d8c683609b144bdfe77087289f8e73df423
brunEdo <edobruno.m@gmail.com>
2021-07-29 14:27:49 +0200

    script

--- js/app.js
+++ js/app.js
@@ -1,8 +1,21 @@
 // TICKET PRICE CALCULATOR
 // ask and store user age
+let userAge = parseInt(prompt("How old are you?"));
 // ask and store distance of the destination
-// store price moltiplicator, minor discount and over 65 discount
+let distance = parseInt(prompt("How far do you want to travel (km)"));
+// store price multiplier, minor discount and over 65 discount
+const priceMultiplier = 0.21;
+const minorDiscount = 0.8;
+const over65Discount = 0.6;
+// calculate base price
+let price = distance * priceMultiplier;
 // if user is a minor apply discount
+if (userAge < 18) {
+    price *= minorDiscount;
+}
 // if user age is over 65 apply discount
-// return the price
+if (userAge >= 65) {
+    price *= over65Discount;
+}
 // show price to the user
+alert("The ticket price is: " + price + "€");
