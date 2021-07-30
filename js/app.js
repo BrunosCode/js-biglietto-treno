dd96208b693cde1b6eb2bd3734d87524259826b4
brunEdo <edobruno.m@gmail.com>
2021-07-30 10:12:05 +0200

    js check for error refactoring

--- js/app.js
+++ js/app.js
@@ -7,30 +7,32 @@ const ticketPrice = () => {
     // check for error
     if (userAge <= 0 || isNaN(userAge)) {
         document.getElementById('age-error').innerHTML = "(Invalid age, retry)";
-    } else if (distance <= 0 || isNaN(distance)) {
-        document.getElementById('age-error').innerHTML = "";
+        return;
+    } 
+    document.getElementById('age-error').innerHTML = "";
+    if (distance <= 0 || isNaN(distance)) {
         document.getElementById('distance-error').innerHTML = "(Invalid distance, retry)";
-    } else {
-        document.getElementById('distance-error').innerHTML = "";
-        // store price multiplier, minor discount and over 65 discount
-        const priceMultiplier = 0.21;
-        const minorDiscount = 0.8;
-        const over65Discount = 0.6;
-        // calculate base price
-        let price = distance * priceMultiplier;
-        // if user is a minor apply discount
-        if (userAge < 18) {
-            price *= minorDiscount;
-        }
-        // if user age is over 65 apply discount
-        if (userAge >= 65) {
-            price *= over65Discount;
-        }
-        // round the price to decimal number
-        price = Math.round(price * 100)/ 100;
-        // show price to the user
-        document.getElementById('ticket-price').innerHTML = price + "€";
+        return;
     }
+    document.getElementById('distance-error').innerHTML = "";
+    // store price multiplier, minor discount and over 65 discount
+    const priceMultiplier = 0.21;
+    const minorDiscount = 0.8;
+    const over65Discount = 0.6;
+    // calculate base price
+    let price = distance * priceMultiplier;
+    // if user is a minor apply discount
+    if (userAge < 18) {
+        price *= minorDiscount;
+    }
+    // if user age is over 65 apply discount
+    if (userAge >= 65) {
+        price *= over65Discount;
+    }
+    // round the price to decimal number
+    price = Math.round(price * 100)/ 100;
+    // show price to the user
+    document.getElementById('ticket-price').innerHTML = price + "€";
 }
 
 
