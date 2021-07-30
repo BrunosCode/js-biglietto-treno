4553e09166a47d6303ee0c4ebdafd8855c8f1159
brunEdo <edobruno.m@gmail.com>
2021-07-30 09:27:36 +0200

    form and style

--- js/app.js
+++ js/app.js
@@ -1,23 +1,37 @@
 // TICKET PRICE CALCULATOR
-// ask and store user age
-let userAge = parseInt(prompt("How old are you?"));
-// ask and store distance of the destination
-let distance = parseInt(prompt("How far do you want to travel (km)"));
-// store price multiplier, minor discount and over 65 discount
-const priceMultiplier = 0.21;
-const minorDiscount = 0.8;
-const over65Discount = 0.6;
-// calculate base price
-let price = distance * priceMultiplier;
-// if user is a minor apply discount
-if (userAge < 18) {
-    price *= minorDiscount;
+const ticketPrice = () => {
+    // ask and store user age
+    const userAge = parseInt(document.getElementById('user-age').value);
+    // ask and store distance of the destination
+    const distance = parseInt(document.getElementById('distance').value);
+    // check for error
+    if (userAge <= 0 || isNaN(userAge)) {
+        document.getElementById('age-error').innerHTML = "(Invalid age, retry)";
+    } else if (distance <= 0 || isNaN(distance)) {
+        document.getElementById('age-error').innerHTML = "";
+        document.getElementById('distance-error').innerHTML = "(Invalid distance, retry)";
+    } else {
+        document.getElementById('distance-error').innerHTML = "";
+        // store price multiplier, minor discount and over 65 discount
+        const priceMultiplier = 0.21;
+        const minorDiscount = 0.8;
+        const over65Discount = 0.6;
+        // calculate base price
+        let price = distance * priceMultiplier;
+        // if user is a minor apply discount
+        if (userAge < 18) {
+            price *= minorDiscount;
+        }
+        // if user age is over 65 apply discount
+        if (userAge >= 65) {
+            price *= over65Discount;
+        }
+        // round the price to decimal number
+        price = Math.round(price * 100)/ 100;
+        // show price to the user
+        document.getElementById('ticket-price').innerHTML = price + "€";
+    }
 }
-// if user age is over 65 apply discount
-if (userAge >= 65) {
-    price *= over65Discount;
-}
-// round the price to decimal number
-price = Math.round(price * 100)/ 100;
-// show price to the user
-alert("The ticket price is: " + price + "€");
+
+
+document.getElementById('submitBtn').addEventListener('click', ticketPrice);
